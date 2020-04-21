a718bd891f3c5031ae5989638b47bab71e7263f5
Luccas Lauth Gianesini <luccas.lauth@gmail.com>
2020-04-21 11:58:32 -0300

    Deimos tracer

--- samples/Solari.Samples.WebApi/Controllers/TestController.cs
+++ samples/Solari.Samples.WebApi/Controllers/TestController.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Elastic.CommonSchema;
 using Microsoft.AspNetCore.Mvc;
+using Solari.Deimos.Abstractions;
 using Solari.Titan;
 
 namespace Solari.Samples.WebApi.Controllers
@@ -11,13 +13,23 @@ namespace Solari.Samples.WebApi.Controllers
     public class TestController : ControllerBase
     {
         private readonly ITitanLogger<TestController> _logger;
+        private readonly IDeimosTracer _tracer;
 
-        public TestController(ITitanLogger<TestController> logger) { _logger = logger; }
+        public TestController(ITitanLogger<TestController> logger, IDeimosTracer tracer)
+        {
+            _logger = logger;
+            _tracer = tracer;
+        }
 
         [HttpGet]
         public IActionResult Get()
         {
+            _tracer.TraceOperation("test-operation");
             _logger.Information("Teste");
+            _tracer.FinalizeTrace(new Dictionary<string, object>
+            {
+                {"Log", "jdalksndaljhdjlasjdlhasldkalhdlasjld"}
+            });
           return  Ok("1");
         } 
 
--- src/Solari.Deimos/SolariBuilderExtensions.cs
+++ src/Solari.Deimos/SolariBuilderExtensions.cs
@@ -13,6 +13,7 @@ namespace Solari.Deimos
         {
             var options = solariBuilder.AppConfiguration.GetOptions<DeimosOptions>(DeimosConstants.TracingAppSettingsSection);
             solariBuilder.Services.Configure<DeimosOptions>(solariBuilder.AppConfiguration.GetSection(DeimosConstants.TracingAppSettingsSection));
+            solariBuilder.Services.AddSingleton<IDeimosTracer, DeimosTracer>();
             ConfigureTracing(solariBuilder, options);
             if (action == null) return solariBuilder;
             var manager = new TracerPluginManager(solariBuilder);
@@ -27,16 +28,7 @@ namespace Solari.Deimos
                 solariBuilder.AddDeimosCorrelationId(options.Http.UseMiddleware);
             }
             JaegerTracerConfiguration.AddJaeger(solariBuilder, options);
-
-            // if (options.UseJaeger && !options.UseElasticApm)
-            // {
-            //     
-            // }
-
-            // if (options.UseElasticApm && !options.UseJaeger)
-            // {
-            //     Solari.Deimos.Elastic.ElasticTracerConfiguration.AddElasticApm(solariBuilder, options);
-            // }
+            
         }
     }
 }
