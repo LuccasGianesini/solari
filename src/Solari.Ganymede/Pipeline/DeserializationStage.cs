b061bb507c245ffe5b77556a32b1e43cd3651691
Luccas Lauth Gianesini <luccas.lauth@gmail.com>
2020-05-30 15:14:17 -0300

    Maybe deserialization methods at ganymede

--- src/Solari.Ganymede/Pipeline/DeserializationStage.cs
+++ src/Solari.Ganymede/Pipeline/DeserializationStage.cs
@@ -9,8 +9,8 @@ namespace Solari.Ganymede.Pipeline
     {
         public static async ValueTask<string> AsString(this ValueTask<GanymedeHttpResponse> httpResponse)
         {
-            GanymedeHttpResponse response = await httpResponse;
-            return await response.AsString();
+            Maybe<string> maybe = await httpResponse.AsMaybeOfString();
+            return maybe.Value;
         }
 
         public static async ValueTask<string> AsString(this GanymedeHttpResponse httpResponse)
@@ -18,19 +18,52 @@ namespace Solari.Ganymede.Pipeline
             return await httpResponse.ResponseMessage.Content.ReadAsStringAsync();
         }
 
-        public static async ValueTask<T> AsModel<T>(this ValueTask<GanymedeHttpResponse> httpResponse)
+        public static async ValueTask<Maybe<string>> AsMaybeOfString(this GanymedeHttpResponse httpResponse)
         {
-            GanymedeHttpResponse response = await httpResponse;
-            return await response.AsModel<T>();
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return Maybe<string>.Some(await httpResponse.AsString());
+            }
+
+            return Maybe<string>.None;
         }
 
-        public static async ValueTask<T> AsModel<T>(this GanymedeHttpResponse httpResponse)
+        public static async ValueTask<Maybe<string>> AsMaybeOfString(this ValueTask<GanymedeHttpResponse> httpResponse)
         {
-            Maybe<T> deserialized = await httpResponse
-                                          .ResponseMessage
-                                          .RequestMessage.GetContentDeserializer()
-                                          .Deserialize<T>(httpResponse.ResponseMessage.Content);
+            GanymedeHttpResponse response = await httpResponse;
+            return await response.AsMaybeOfString();
+        }
 
+        public static async ValueTask<Maybe<T>> AsMaybeOf<T>(this ValueTask<GanymedeHttpResponse> httpResponse)
+        {
+            GanymedeHttpResponse response = await httpResponse;
+            return await response.AsMaybeOf<T>();
+        }
+        public static async ValueTask<Maybe<T>> AsMaybeOf<T>(this GanymedeHttpResponse httpResponse)
+        {
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return await httpResponse
+                             .ResponseMessage
+                             .RequestMessage.GetContentDeserializer()
+                             .Deserialize<T>(httpResponse.ResponseMessage.Content);
+
+            }
+
+            return Maybe<T>.None;
+        }
+
+        public static async ValueTask<T> As<T>(this ValueTask<GanymedeHttpResponse> httpResponse)
+        {
+            GanymedeHttpResponse response = await httpResponse;
+            return await response.As<T>();
+        }
+
+        public static async ValueTask<T> As<T>(this GanymedeHttpResponse httpResponse)
+        {
+            Maybe<T> deserialized = await httpResponse.ResponseMessage
+                                                      .RequestMessage.GetContentDeserializer()
+                                                      .Deserialize<T>(httpResponse.ResponseMessage.Content);
             return deserialized.Value;
         }
     }
